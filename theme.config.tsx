ec331ac0b779d38f5165d026c166738d6a528d2b
Sounho Chung <aschung01@snu.ac.kr>
2023-10-13 10:50:57 +0900

    ADD : Added getting-started, init migration guides

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>PromptModel</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/weavel-fastllm/fastllm",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/weavel-fastllm/fastllm",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "PromptModel © 2023 by Weavel Inc.",
   },
-}
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s - PromptModel",
+      };
+    }
+  },
+};
 
-export default config
+export default config;
